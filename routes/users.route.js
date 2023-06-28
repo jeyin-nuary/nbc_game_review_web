28b6a0974a8ecf1597f5c17dabf6336aaef38192
helloiamnoob <86592051+helliamnoob@users.noreply.github.com>
2023-06-28 11:48:48 +0900

    로그인 api, 토큰확인 미들웨어 추가
    로그인 api와 토큰미들웨어 추가
    [테스트필요-auth미들웨어]_회원가입, 로그인까지는 문제없으나 토큰미들웨어 작동은 게시판 같은 다른 api 추가 후 테스트 필요

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const {Users} = require("../models");
 const router = express.Router();
 
 
 //회원가입 API
-router.post("/users", async(req, res) => {
+router.post("/signup", async(req, res) => {
     const{login_id, password, nickname, age} =req.body;
     console.log(Users);
     const isExitsUser = await Users.findOne({
@@ -22,5 +23,26 @@ router.post("/users", async(req, res) => {
     return res.status(201).json({ message: "회원가입이 완료되었습니다."})
 })
 
+router.post("/login", async(req,res)=> {
+    const { login_id, password} = req.body;
+    const user = await Users.findOne({
+        where: {login_id}
+    });
+
+    if(!user){
+        return res.status(401).json({message: "해당하는 사용자가 존재하지 않습니다."});
+    } else if(user.password !== password){
+        return res.status(401).json({message:"비밀번호가 일치하지 않습니다."});
+    }
+
+    const token = jwt.sign({
+        userId: user.user_id
+    }, "customized_secret_key");
+
+    //쿠키발급
+    res.cookie("authorization", `Bearer ${token}`);
+    //response 할당
+    return res.status(200).json({message:"로그인에 성공하였습니다."})
+});
 
 module.exports = router;
