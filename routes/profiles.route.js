0c50749a46bd2c8760b64bcc025910b0b5ad5f85
yerim <sola4718@gmail.com>
2023-06-29 18:11:26 +0900

    수정기능 완료, 비밀번호 수정기능 500번 오류

--- routes/profiles.route.js
+++ routes/profiles.route.js
@@ -23,25 +23,23 @@ router.get('/profiles/:login_id', authMiddleware, async (req, res) => {
 
 // 프로필 수정
 router.put('/profiles/:login_id', authMiddleware, async (req, res) => {
-  const { userId } = req.params;
-  const { user } = res.locals;
-  const { password, nickname, introduction } = req.body;
+  const { login_id } = req.params;
+  const { user_id } = res.locals.user;
+  const { password, nickname } = req.body;
 
-  if (user.user_id !== Number(userId)) {
+  const user = await Users.findOne({
+    where: { user_id }
+  });
+
+  if (user.login_id !== login_id) {
     return res.status(403).json({ message: "프로필을 수정할 권한이 없습니다." });
-  }
-
-  // 비밀번호 인증
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    return res.status(401).json({ message: "비밀번호가 올바르지 않습니다." });
   }
 
   try {
     // 이름, 한 줄 소개 등 프로필 정보 수정
     await Users.update(
-      { nickname, introduction },
-      { where: { user_id: userId } }
+      { nickname },
+      { where: { user_id: user_id } }
     );
 
     res.status(200).json({ message: "프로필이 성공적으로 수정되었습니다." });
@@ -51,29 +49,37 @@ router.put('/profiles/:login_id', authMiddleware, async (req, res) => {
 });
 
 // 비밀번호 수정
-router.put('/profiles/:userId/password', authMiddleware, async (req, res) => {
-  const { userId } = req.params;
-  const { user } = res.locals;
-  const { currentPassword, newPassword } = req.body;
+router.put('/profiles/:login_id/password', authMiddleware, async (req, res) => {
+  const { login_id } = req.params;
+  const { user_id } = res.locals.user;
+  const { newPassword, confirmPassword, currentPassword } = req.body;
 
-  if (user.user_id !== Number(userId)) {
+  const user = await Users.findOne({
+    where: { user_id }
+  });
+
+  if (user.login_id !== login_id) {
     return res.status(403).json({ message: "비밀번호를 수정할 권한이 없습니다." });
   }
 
-  // 현재 비밀번호 인증
-  const isCurrentPasswordValid = await bcrypt.compare(currentPassword, user.password);
-  if (!isCurrentPasswordValid) {
-    return res.status(401).json({ message: "현재 비밀번호가 올바르지 않습니다." });
+  if (newPassword !== confirmPassword) {
+    return res.status(400).json({ message: "새로운 비밀번호와 확인 비밀번호가 일치하지 않습니다." });
   }
 
   try {
+    // 현재 비밀번호 인증
+    const isCurrentPasswordValid = await bcrypt.compare(currentPassword, user.password);
+    if (!isCurrentPasswordValid) {
+      return res.status(401).json({ message: "현재 비밀번호가 올바르지 않습니다." });
+    }
+
     // 새로운 비밀번호 해싱
     const hashedNewPassword = await bcrypt.hash(newPassword, 10);
 
     // 비밀번호 업데이트
     await Users.update(
       { password: hashedNewPassword },
-      { where: { user_id: userId } }
+      { where: { user_id: user_id } }
     );
 
     res.status(200).json({ message: "비밀번호가 성공적으로 수정되었습니다." });
@@ -81,6 +87,8 @@ router.put('/profiles/:userId/password', authMiddleware, async (req, res) => {
     res.status(500).json({ message: "비밀번호 수정 중에 오류가 발생했습니다." });
   }
 });
+
+
 
 module.exports = router;
 
