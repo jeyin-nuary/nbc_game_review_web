0f427865be8fe1cd2b20666ab72daf9eee9f6f59
helloiamnoob <86592051+helliamnoob@users.noreply.github.com>
2023-06-29 16:03:32 +0900

    fix) 미들웨어 오류 수정
    token에 담기는 data값의 key명이 달라서 테이블에서 불러오지 않는 에러가 발생, token 값 내 key 명 수정 및 테이블에 조회하는 변수명 재설정
    + 프로필 조회만 수정완료, 수정이 되는지 확인필요

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -3,8 +3,8 @@ const { Users } = require("../models");
 
 module.exports = async (req, res, next) => {
   try {
-    const { authorization } = req.cookies;
-    const [tokenType, token] = authorization.split(" ");
+    const { Authorization } = req.cookies;
+    const [tokenType, token] = Authorization.split(" ");
     if (tokenType !== "Bearer") {
       return res.status(401).json({ message: "토큰 타입이 일치하지 않습니다." });
     }
@@ -14,15 +14,14 @@ module.exports = async (req, res, next) => {
 
     const user = await Users.findOne({ where: { user_id } });
     if (!user) {
-      res.clearCookie("authorization");
+      res.clearCookie("Authorization");
       return res.status(401).json({ message: "토큰 사용자가 존재하지 않습니다." });
     }
     res.locals.user = user;
 
     next();
-
   } catch (error) {
-    res.clearCookie("authorization");
+    res.clearCookie("Authorization");
     return res.status(401).json({
       message: "비정상적인 요청입니다."
     });
