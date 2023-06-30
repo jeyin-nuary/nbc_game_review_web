d5cc7b032dde97d4a60b1daa3ed07b9d2f0c29ec
yerim <sola4718@gmail.com>
2023-06-30 17:47:38 +0900

    프로필 수정과 비밀번호에 대한 유효성 검사 추가

--- routes/profiles.route.js
+++ routes/profiles.route.js
@@ -25,7 +25,7 @@ router.get('/profiles/:login_id', authMiddleware, async (req, res) => {
 router.put('/profiles/:login_id', authMiddleware, async (req, res) => {
   const { login_id } = req.params;
   const { user_id } = res.locals.user;
-  const { password, nickname } = req.body;
+  const { password, nickname, age, email, introduction  } = req.body;
 
   const user = await Users.findOne({
     where: { user_id }
@@ -36,9 +36,18 @@ router.put('/profiles/:login_id', authMiddleware, async (req, res) => {
   }
 
   try {
+    // 유효성 검사
+    if (email && !email.includes('@')) {
+      return res.status(400).json({ message: "이메일 형식이 아닙니다." });
+    }
+
+    if (introduction && introduction.length > 30) {
+      return res.status(400).json({ message: "한 줄 소개는 30자 이하여야 합니다." });
+    }
+
     // 이름, 한 줄 소개 등 프로필 정보 수정
     await Users.update(
-      { nickname },
+      { nickname, age, email, introduction },
       { where: { user_id: user_id } }
     );
 
@@ -49,6 +58,7 @@ router.put('/profiles/:login_id', authMiddleware, async (req, res) => {
 });
 
 // 비밀번호 수정
+
 router.put('/profiles/:login_id/password', authMiddleware, async (req, res) => {
   const { login_id } = req.params;
   const { user_id } = res.locals.user;
@@ -65,15 +75,23 @@ router.put('/profiles/:login_id/password', authMiddleware, async (req, res) => {
   if (newPassword !== confirmPassword) {
     return res.status(400).json({ message: "새로운 비밀번호와 확인 비밀번호가 일치하지 않습니다." });
   }
+  
+// 비밀번호 유효성 검사
+
+let scriptTagPwd = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+
+if (password.length == 0) {
+  return res.status(400).json({ message: "비밀번호를 입력해주세요." });
+} else if (newPassword.length < 8) {
+  return res.status(400).json({ message: "비밀번호는 8자리 이상입니다." });
+} else if (scriptTagPwd.test(newPassword) == false) {
+  return res.status(400).json({ message: "비밀번호에는 영문, 숫자, 특수문자가 포함되어야 합니다." });
+} else if (newPassword.includes(login_id)) {
+  return res.status(400).json({ message: "비밀번호에는 아이디가 들어갈 수 없습니다." });
+}
+
 
   try {
-
-      // 원래는 bcrypt()를 사용해서 인증하려고 했지만 자꾸 오류가 생겨서 직접 변경으로 수정함
-      // const isCurrentPasswordValid = await bcrypt.compare(password, user.password);
-      // if (!isCurrentPasswordValid) {
-      //   return res.status(401).json({ message: "비밀번호가 올바르지 않습니다." });
-      // }
-  
     // 현재 비밀번호 인증
     if (password !== user.password) {
       console.log('현재 비밀번호가 올바르지 않습니다.'); // 콘솔 로그 출력
@@ -93,10 +111,9 @@ router.put('/profiles/:login_id/password', authMiddleware, async (req, res) => {
   } catch (error) {
     console.error('비밀번호 수정 중에 오류가 발생했습니다:', error); // 콘솔 에러 로그 출력
     res.status(500).json({ message: "비밀번호 수정 중에 오류가 발생했습니다." });
+
   }
 });
-
-
 
 module.exports = router;
 
